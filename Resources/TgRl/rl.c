c5070b6027ee7c5b4bc6de09f5c62fe4b6c7217d
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-11-24 19:53:20 +0300

    TgRl

--- Resources/TgRl/rl.c
+++ Resources/TgRl/rl.c
@@ -9,6 +9,24 @@ typedef struct
 	void (*callback)(void);
 } TickGeneratorInfo;
 
+void SetPriorityRealtime()
+{
+    mach_timebase_info_data_t timebase;
+    kern_return_t kr = mach_timebase_info(&timebase);
+
+    // Set the thread priority.
+    thread_time_constraint_policy ttcpolicy;
+    thread_port_t threadport = pthread_mach_thread_np(pthread_self());
+
+    // In ticks. Therefore to convert nanoseconds to ticks multiply by (timebase.denom / timebase.numer).
+    ttcpolicy.period = 500 * 1000 * timebase.denom / timebase.numer; // Period over which we demand scheduling.
+    ttcpolicy.computation = 100 * 1000 * timebase.denom / timebase.numer; // Minimum time in a period where we must be running.
+    ttcpolicy.constraint = 100 * 1000 * timebase.denom / timebase.numer; // Maximum time between start and end of our computation in the period.
+    ttcpolicy.preemptible = FALSE;
+
+    kr = thread_policy_set(threadport, THREAD_TIME_CONSTRAINT_POLICY, (thread_policy_t)&ttcpolicy, THREAD_TIME_CONSTRAINT_POLICY_COUNT);
+}
+
 void TimerCallBack(CFRunLoopTimerRef timer, void *info)
 {
 	TickGeneratorInfo* tickGeneratorInfo = (TickGeneratorInfo*)info;
@@ -33,6 +51,7 @@ void* RunLoopThreadRoutine(void* data)
 	CFRunLoopAddTimer(runLoopRef, timerRef, kCFRunLoopDefaultMode);
 	
     tickGeneratorInfo->active = 1;
+	SetPriorityRealtime();
 
     CFRunLoopRun();
 
