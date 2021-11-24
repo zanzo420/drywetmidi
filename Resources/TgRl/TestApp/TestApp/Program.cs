b3f2c89a3fde65d49b0d345519013b48d51703fb
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-11-24 19:41:44 +0300

    TgRl

--- Resources/TgRl/TestApp/TestApp/Program.cs
+++ Resources/TgRl/TestApp/TestApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -12,18 +15,75 @@ namespace TestApp
         private static extern void CreateTimer(Callback callback);
 
         private static Callback _callback;
+        private static Stopwatch _stopwatch;
+        private static List<long> _times;
 
         static void Main(string[] args)
         {
             _callback = CallbackImpl;
-            CreateTimer(_callback);
 
-            Thread.Sleep(10000);
+            _times = new List<long>((int)Math.Round(MeasurementDuration.TotalMilliseconds) + 1);
+            _stopwatch = new Stopwatch();
+
+            CreateTimer(_callback);
+            _stopwatch.Start();
+
+            Thread.Sleep(MeasurementDuration);
+
+            _stopwatch.Stop();
+
+            var deltas = new List<long>();
+            var lastTime = 0L;
+
+            foreach (var time in _times)
+            {
+                var delta = time - lastTime;
+                deltas.Add(delta);
+
+                lastTime = time;
+            }
+
+            var min = deltas.Min();
+            var max = deltas.Max();
+            var average = deltas.Average();
+
+            var areaSize = 1 * GoodAreaPercents / 100;
+
+            var result = new Result(
+                _times.Count,
+                _times[0],
+                min,
+                deltas.Count(d => d == min) / (double)deltas.Count * 100,
+                max,
+                deltas.Count(d => d == max) / (double)deltas.Count * 100,
+                average,
+                deltas.Count(d => d >= 1 - areaSize && d <= 1 + areaSize) / (double)deltas.Count * 100);
+
+            Console.WriteLine($"Results on {result.TimesCount} times:");
+            Console.WriteLine($"    first   = {result.First}");
+            Console.WriteLine($"    min     = {result.Min} ({result.MinPercent:0.##} %)");
+            Console.WriteLine($"    max     = {result.Max} ({result.MaxPercent:0.##} %)");
+            Console.WriteLine($"    average = {result.Average:0.##}");
+            Console.WriteLine($"    good    = {result.FivePercentsIntervalAreaPercent:0.##} %");
         }
 
         private static void CallbackImpl()
         {
-            Console.WriteLine("AAA");
+            _times.Add(_stopwatch.ElapsedMilliseconds);
         }
+
+        private record Result(
+            int TimesCount,
+            long First,
+            long Min,
+            double MinPercent,
+            long Max,
+            double MaxPercent,
+            double Average,
+            double FivePercentsIntervalAreaPercent);
+
+        private const double GoodAreaPercents = 10.0;
+        private static readonly TimeSpan MeasurementDuration = TimeSpan.FromSeconds(30);
+        private static readonly int[] IntervalsToCheck = { 1, 10, 100 };
     }
 }
